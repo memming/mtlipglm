68b457326022f9f7d9e1b0219b2ebef5bedf1025
Jacob Yates <jacoby8s@gmail.com>
2017-06-07 01:43:23 -0400

    ready for internal share

--- analysis_scripts/cmdFitMT.m
+++ analysis_scripts/cmdFitMT.m
@@ -1,24 +1,35 @@
+% Fit GLM to MT units
+% This code calls the fitting code and analysis code for each LIP unit in
+% the dataset. The fits are regularized using ridge-regression. This code
+% uses a fixed ridge parameter (set below)
+% All analyses are handled by the mtlipglm class. The design/fitting of
+% each GLM is handled by the glmspike class, which is an instance of the
+% neuroGLM class available at https://github.com/jcbyts/neuroGLM
+dataPath = getpref('mtlipglm', 'dataPath');
 
+% --- Find experiments that have multiple MT units
 experiments=getExperimentsAnd({'MT', 'simultaneous'});
+regenerateModelComparisonFiles = true; % regenerate model comparison files from fits
+ridgeParameter = 0.1; % regularize fits with ridge-regression (L2 penatly)
+refitModel     = false; % if true, fits will be overwritten
 
-regenerateModelComparisonFiles=true;
-% experiments = {'n20150306c'};
+% Parfor if possible, change to for-loop if you do not have the parallel
+% toolbox
 parfor kExperiment=1:numel(experiments)
+    
+    % --- setup analysis for each session
     exname=experiments{kExperiment};
-    mstruct=mtlipglm();
-    mstruct.overwrite=false;
-    mstruct.nFolds=5;
-    mstruct.regressionMode='RidgeGroupCV';
-    mstruct.binSize=10;
-    mstruct.modelDir=fullfile('data','glmFitsMain5');
-    mstruct.getExperiment(exname)
-	mstruct.buildTrialStruct('IncludeContrast', true, 'MotionEpoch', true, 'MTsim', false);
-    for k=1:numel(mstruct.trial)
-        mstruct.trial(k).gaborContrast=mstruct.trial(k).contrasts;
-    end
+    mstruct=mtlipglm(exname, dataPath, 'overwrite', refitModel, 'nFolds', 5);
+    mstruct.overwrite =refitModel;
+    mstruct.binSize = 10; % Don't change. Analysis code assumes binSize=10
+    mstruct.modelDir = fullfile(dataPath,'main_fits');
+    % buildTrialStruct builds a struct array of each trial's data in the
+    % format that is used by neuroGLM. This is way faster than the LIP fits
+    % because there is no need to simulate MT input ('MTsim', false)
+    mstruct.buildTrialStruct('IncludeContrast', true, 'MotionEpoch', true, 'MTsim', false);
     
     for kNeuron=1:numel(mstruct.neurons)
-        if ~mstruct.neurons(kNeuron).isMT % skip
+        if ~mstruct.neurons(kNeuron).isMT % skip LIP
             continue
         end
         fname=fullfile(mstruct.modelDir, [mstruct.neurons(kNeuron).getName 'modelComparison.mat']);
@@ -27,7 +38,14 @@ parfor kExperiment=1:numel(experiments)
             fprintf('\n\n\nSkipping [%s] \n\n\n\n\n', fname)
             continue
         else
-            g=fitAllModels(mstruct,kNeuron,true,1:4,'instantaneousCoupling', true, 'rho', [.8 1 2 5 10 20]);
+            % --- Fitting happens here
+            % Specify which models to fit:
+            % 1 - Stimulus-to-LIP
+            % 2 - Stimulus-to-LIP w/ history filter
+            % 3 - Inter-Area Coupling
+            % 4 - Intra-Area Coupling
+            modelIx = 1:4;
+            g=fitAllModels(mstruct,kNeuron,true,modelIx,'instantaneousCoupling', true, 'rho', ridgeParameter);
             P=mstruct.modelComparisonToo(g);
             parsave(fname, P,'-v7')
         end
